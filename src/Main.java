d52506a8dbe456ff7e2b8d1079d41716f0b400d5
litluke227 <lucasm.lm95@gmail.com>
2025-03-09 22:56:45 -0400

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+        Random rnd = new Random();
+        do {
+            int die1 = rnd.nextInt(6) + 1;
+            int die2 = rnd.nextInt(6) + 1;
+            int dieSum = die1 + die2;
+            System.out.println("The dice are " + die1 + " and " + die2);
+            if (dieSum == 2 || dieSum == 3 || dieSum == 12) {
+                System.out.println("Craps! You lose");
+            }
+            if (dieSum == 7 || dieSum == 11) {
+                System.out.println("With a sum of " + dieSum + ", you WIN with a natural!!!");
+            } else {
+                int point = dieSum;
+                System.out.println("The point you are trying to hit is: " + point);
+                while (true) {
+                    int dieRoll1 = rnd.nextInt(6) + 1;
+                    int dieRoll2 = rnd.nextInt(6) + 1;
+                    int dieSum2 = dieRoll1 + dieRoll2;
+                    System.out.println("You rolled " + dieRoll1 + " and " + dieRoll2);
+                    if (dieSum2 == point) {
+                        System.out.println("You win by matching the point!");
+                        break;
+                    } else if (dieSum2 == 7) {
+                        System.out.println("You lose by rolling a 7");
+                        break;
+                    }
+                }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+                System.out.println("Do you want to play again?");
+                String confirm = in.nextLine();
+                done = !confirm.equalsIgnoreCase("Y");
+
+
+            }
+        } while (!done);
     }
 }
